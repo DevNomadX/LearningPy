c3410cdfb9b938fc3985e29200de85f36859cfb2
DevNomadX <skmonir1205909@gmail.com>
2024-02-12 21:01:33 +0600

    adding some new learning carv

--- student.py
+++ student.py
@@ -1,13 +1,23 @@
 # 57m:00s
 class Student:
     def __init__(self, name, house) -> None:
+        if not name:
+            raise ValueError("Missing Name")
+
+        if house not in ["Jessore", "Khulna", "Bangladesh"]:
+            raise ValueError("Invalid House")
+
         self.name = name
         self.house = house
+
+    def __str__(self) -> str:
+        return f"{self.name} from {self.house}"
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    # print(f"{student.name} from {student.house}")
+    print(student)
 
 
 def get_student():
